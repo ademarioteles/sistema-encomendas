e3fb4080168c89e1246aa022a744c35d3ab7fb35
Ademario <ademarioteles@hotmail.com>
2023-05-07 01:39:55 -0300

    Tornando o indexPage responsivo

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,49 +1,52 @@
 <template>
-  <div class="bg-gray" style="max-width: 300px">
-    <q-input
-      filled
-      v-model="ph"
-      label="CPF:"
-      placeholder="000.000.000-01"
-      color="blue"
-      :dense="dense"
-    />
+  <div class="container bg-white">
+    <div class="bg-gray" style="max-width: 300px">
+      <h5><b>ACESSAR SISTEMA</b></h5>
+      <q-input
+        filled
+        v-model="cpf"
+        label="CPF:"
+        placeholder="000.000.000-01"
+        color="teal"
+        :dense="dense"
+      />
 
-    <q-radio
-      keep-color
-      v-model="color"
-      val="porsin"
-      label="Porteiro/Sindico"
-      color="teal"
-      @click="buscarRadio()"
-    />
-    <q-radio
-      keep-color
-      v-model="color"
-      val="inq"
-      label="Inquilino"
-      color="teal"
-      @click="buscarRadio()"
-    />
-    <q-input
-      filled
-      v-model="ph"
-      label="PASSWORD:"
-      placeholder="000.000.000-01"
-      :dense="dense"
-    />
-    <q-row>
-      <q-col cols="auto">
-        <q-btn
-          val="Entrar"
-          unelevated
-          rounded
-          color="secondary"
-          label="Entrar"
-        />
-      </q-col>
-    </q-row>
-    <br />
+      <q-radio
+        keep-color
+        v-model="tipoPessoa"
+        val="porsin"
+        label="Porteiro/Sindico"
+        color="teal"
+      />
+      <q-radio
+        keep-color
+        v-model="tipoPessoa"
+        val="inq"
+        label="Inquilino"
+        color="teal"
+      />
+      <q-input
+        filled
+        v-model="ph"
+        color="teal"
+        label="PASSWORD:"
+        placeholder="000.000.000-01"
+        :dense="dense"
+      />
+      <q-row>
+        <q-col cols="auto">
+          <q-btn
+            val="Entrar"
+            unelevated
+            rounded
+            color="black"
+            label="Entrar"
+            @click="buscarRadio()"
+          />
+        </q-col>
+      </q-row>
+      <br />
+    </div>
   </div>
 </template>
 
@@ -54,9 +57,10 @@ export default defineComponent({
   name: "IndexPage",
   setup() {
     return {
-      color: ref("cyan"),
-      buscarRadio(valor) {
-        alert("hello");
+      cpf: ref(""),
+      tipoPessoa: ref(""),
+      buscarRadio() {
+        alert(this.cpf);
       },
     };
   },

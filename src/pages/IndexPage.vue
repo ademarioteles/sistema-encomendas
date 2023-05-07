6411528c8b90f23008c13b44489a5993eff36a9c
Ademario <ademarioteles@hotmail.com>
2023-05-06 21:04:09 -0300

    Trabalho em progresso

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,51 @@
 <template>
-  <h1>Primeiro Commit</h1>
+  <div class="bg-gray" style="max-width: 300px">
+    <q-input
+      filled
+      v-model="ph"
+      label="CPF:"
+      placeholder="000.000.000-01"
+      :dense="dense"
+    />
+
+    <q-radio
+      keep-color
+      v-model="color"
+      val="porsin"
+      label="Porteiro/Sindico"
+      color="teal"
+      @click="buscarRadio()"
+    />
+    <q-radio
+      keep-color
+      v-model="color"
+      val="inq"
+      label="Inquilino"
+      color="teal"
+      @click="buscarRadio()"
+    />
+    <q-input
+      filled
+      v-model="ph"
+      label="CPF:"
+      placeholder="000.000.000-01"
+      :dense="dense"
+    />
+  </div>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "IndexPage",
+  setup() {
+    return {
+      color: ref("cyan"),
+      buscarRadio(valor) {
+        alert();
+      },
+    };
+  },
 });
 </script>
